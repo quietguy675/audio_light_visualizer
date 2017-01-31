424c447ccfba1e4ef7f281c3a70697ae8f22dd22
Matt From <matfrom@gmail.com>
2017-01-31 00:39:05 -0600

    updated code with rotating colors and sequencer.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # audio_light_visualizer
 uses the spectrum shield, some neopixels, and a esp8266 or arduino to do a little light show
+
+Use the Spectrum_shield arduino sketch, the library_thing sketch is not working.
--- Spectrum_Shield/Spectrum_Shield.ino
+++ Spectrum_Shield/Spectrum_Shield.ino
@@ -1,11 +1,18 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
 #define STROBE_PIN 4
 #define RESET_PIN 5
-#define LEFT_DATA_PIN A0
-#define RIGHT_DATA_PIN A1
-#define NEOPIXEL_PIN 3
+#define NEOPIXEL_PIN 6
+#define NUM_NEOPIXELS 50
 #define NUM_FREQ 7
+int LEFT_DATA_PIN=14;
+int RIGHT_DATA_PIN=15;
 int frequencies_left[NUM_FREQ];
 int frequencies_right[NUM_FREQ];
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_NEOPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pinMode(STROBE_PIN, OUTPUT);
@@ -17,17 +24,70 @@ void setup() {
   digitalWrite(RESET_PIN, HIGH);
   Serial.begin(9600);
   Initialize_Spectrum_Analyzers();
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
-  Read_Frequencies();
-  delay(50);
+  /*
+  for (int i = 0; i < NUM_FREQ; i++){
+    Serial.print("Freq ");
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(frequencies_left[i]);
+    Serial.print("\t");
+    Serial.print(frequencies_right[i]);
+    Serial.println();
+  }*/
+  update_neopixels(50);
 }
 
-void Read_Frequencies(){
+
+void update_neopixels(int wait){
+  int rescaled_value = 0;
+  for(int k=0; k<256; k++) {
+    Read_Frequencies(frequencies_left, frequencies_right);
+    
+    for (int i = 0; i < NUM_FREQ-2; i++){
+      rescaled_value = map(frequencies_left[i]*1.25, 0, 1023, 0, 9);
+      for (int j = 0; j <= rescaled_value; j++){
+        strip.setPixelColor((i*10)+j, Wheel(k & 255));
+      }
+      for (int j = rescaled_value; j < 10; j++){
+        strip.setPixelColor((i*10)+j, strip.Color(0,0,0));
+      }
+    }
+    strip.show();
+    delay(wait);
+  }
+  
+}
+
+void display_neopixels(int freqs[NUM_FREQ], int wait){
+  int rescaled_value = 0;
+  uint16_t i, j;
+
+  for(int k=0; k<256; k++) {
+    for (int i = 0; i < NUM_FREQ-2; i++){
+      rescaled_value = map(freqs[i]*1.25, 0, 1023, 0, 9);
+      for (int j = 0; j <= rescaled_value; j++){
+        strip.setPixelColor((i*10)+j, Wheel(k & 255));
+      }
+      for (int j = rescaled_value; j < 10; j++){
+        strip.setPixelColor((i*10)+j, strip.Color(0,0,0));
+      }
+    }
+    strip.show();
+    delay(wait);
+  }
+  
+}
+
+
+void Read_Frequencies(int freq_left[7], int freq_right[7]){
   for(int i = 0; i < NUM_FREQ; i++){
-    frequencies_left[i] = analogRead(LEFT_DATA_PIN);
-    frequencies_right[i] = analogRead(RIGHT_DATA_PIN);
+    freq_left[i] = analogRead(LEFT_DATA_PIN);
+    freq_right[i] = analogRead(RIGHT_DATA_PIN);
     digitalWrite(STROBE_PIN, HIGH);
     digitalWrite(STROBE_PIN, LOW);
   }
@@ -45,4 +105,17 @@ void Initialize_Spectrum_Analyzers(){
   digitalWrite(RESET_PIN, LOW);
 }
 
-
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
